e3989317c3d79a635fb7d8785cbc4c1e18fb0a7b
ren328 <57877581+ren328@users.noreply.github.com>
2023-06-05 10:17:51 -0500

    Update varb_1.py

--- varb_1.py
+++ varb_1.py
@@ -8,11 +8,15 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
 ##batch number
-batch=1 
+i=
 ##Use proper function to load $Var(Y_(b)|X,X^*)$
 varb=np.load("path")
 ##Use proper function to load $\hat_sigma(b)^2$
 sigmab="path"
+sigmab=np.array(sigmab)
+##Use proper functin to load SNP matrix
+snp="path"
+snp=np.array(snp)
 
 vary=np.diag(varb)
 vary1=np.diag(1/np.sqrt(vary))
@@ -20,29 +24,6 @@ vary1=np.diag(1/np.sqrt(vary))
 cp=np.matmul(vary1,varb)
 R=np.matmul(cp,vary1)
 
-
-
-
-
-
-##centered SNP matrix 
-r = robjects.r
-r['source']('/panfs/jay/groups/20/panwei/ren00075/0515newvar/20000py/batch1/f1.R')
-rfunction = robjects.globalenv['f1']
-with localconverter(robjects.default_converter + pandas2ri.converter):
-     snp= rfunction(chr)
-
-r = robjects.r
-r['source']('/panfs/jay/groups/20/panwei/ren00075/0515newvar/20000py/batch1/f1.R')
-rfunction = robjects.globalenv['f2']
-with localconverter(robjects.default_converter + pandas2ri.converter):
-     sigma= rfunction(chr)
-
-snp=np.array(snp)
-sigma=np.array(sigma)
-print(sigma.shape)
-print(snp.shape)
-print(type(snp))
 
 p=snp.shape[1]
 ##D,p*p
@@ -52,11 +33,12 @@ for j in range(p):
   c=snp[:,j]
   cp=np.matmul(c.T,c)
   cp1=1/cp
-  cp2=cp1*c.T*sigma[j]
-  cp3=np.matmul(cp2,rb)
+  cp2=cp1*c.T*sigmab[j]
+  cp3=np.matmul(cp2,R)
   cp4=np.matmul(cp3,c)
   cp5=cp4*cp1
   res.append(cp5)
 
-np.savetxt("/panfs/jay/groups/20/panwei/ren00075/0515newvar/20000py/batch1/var{}.txt".format(chr),res)
+##Use proper function to save the result
+np.savetxt("path",res)
 
